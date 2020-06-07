338e36c80e25f7d536e66a4ef3b418b62c91b0a6
Kim <kimwashere@gmail.com>
2020-06-07 16:34:01 +1000

    Adds the code that loads a suburb in

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-
+import path from "path";
 export default {
   mode: 'spa',
   /*
@@ -63,7 +63,13 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
-    }
+    extend(config, ctx) {  
+      config.module.rules.push(
+      {
+          test: /\.md$/,
+          loader: "frontmatter-markdown-loader",
+          include: path.resolve(__dirname, "content/suburb")
+      })
+    }    
   }
 }
---
+++ pages/suburb/_slug.vue
@@ -0,0 +1,23 @@
+<template>
+  <article class="suburb">
+    <div class="suburb__info">
+      <h1>{{ suburb.attributes.name }}</h1>
+      <div>Status: {{ suburb.attributes.status }}</div>
+      <div v-html="suburb.html"></div>
+    </div>
+  </article>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      suburb: {},
+      selectedArticle: null
+    }
+  },
+  created () {
+    const suburb = require(`~/content/suburb/${this.$route.params.slug}.md`)
+    this.suburb = suburb
+  }
+}
+</script>
